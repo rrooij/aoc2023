5cb5a85d84fbc039f9c4da5a818ec7bea846cb38
Robin de Rooij <robin@terminusdb.com>
2023-12-08 00:09:07 +0100

    day5: use rustfmt

--- day_5/src/bin/part1.rs
+++ day_5/src/bin/part1.rs
@@ -3,54 +3,63 @@ use std::ops::Range;
 
 #[derive(Debug)]
 struct SeedMap {
-    mapping: Vec<(Range<u64>, Range<u64>)>
+    mapping: Vec<(Range<u64>, Range<u64>)>,
 }
 
 fn find_lowest(taken_seeds: Vec<u64>, maps: Vec<SeedMap>) {
-    let result: u64 = taken_seeds.iter().map(|seed| {
-        let mut result = *seed;
-        for map in &maps {
-            for (target, source) in &map.mapping {
-                let start = source.start;
-                let end = source.end;
-                if result < start || result >= end {
-                    continue
+    let result: u64 = taken_seeds
+        .iter()
+        .map(|seed| {
+            let mut result = *seed;
+            for map in &maps {
+                for (target, source) in &map.mapping {
+                    let start = source.start;
+                    let end = source.end;
+                    if result < start || result >= end {
+                        continue;
+                    }
+                    result = target.start + (result - start);
+                    break;
                 }
-                result = target.start + (result - start);
-                break;
             }
-        }
-        result
-        }).min().unwrap();
+            result
+        })
+        .min()
+        .unwrap();
     println!("The minimum is {}", result)
 }
 
 fn main() {
     let contents = fs::read_to_string("./input.txt").unwrap();
     let splitted: Vec<&str> = contents.lines().collect();
-    let seeds: Vec<u64> = splitted[0].split(' ')
+    let seeds: Vec<u64> = splitted[0]
+        .split(' ')
         .skip(1)
         .map(|x| x.parse::<u64>().unwrap())
         .collect();
-    let maps: Vec<SeedMap> = contents.split("\n\n").skip(1)
+    let maps: Vec<SeedMap> = contents
+        .split("\n\n")
+        .skip(1)
         .map(|raw_map| {
             let splitted_colon_map: Vec<&str> = raw_map.split(':').collect();
             let numbers: Vec<(Range<u64>, Range<u64>)> = splitted_colon_map[1]
                 .split('\n')
                 .filter(|x| x != &"")
                 .map(|x| {
-                    let number_line: Vec<u64> = x.split(' ').map(|y| y.parse::<u64>().unwrap()).collect();
+                    let number_line: Vec<u64> =
+                        x.split(' ').map(|y| y.parse::<u64>().unwrap()).collect();
                     let range = number_line[2];
                     let seed_range: Range<u64> = number_line[1]..number_line[1] + range;
                     let target_range: Range<u64> = number_line[0]..number_line[0] + range;
                     (target_range, seed_range)
-                }).collect();
-                /*
-                .fold(vec![], move |acc: Vec<Range<u64>>, x| {
-                    println!("Folding!");
-//                    vec![vec![], vec![]]
-                    vec![[acc[0].clone(), x[0].clone()].concat(), [acc[1].clone(), x[1].clone()].concat()]
-        });*/
+                })
+                .collect();
+            /*
+                            .fold(vec![], move |acc: Vec<Range<u64>>, x| {
+                                println!("Folding!");
+            //                    vec![vec![], vec![]]
+                                vec![[acc[0].clone(), x[0].clone()].concat(), [acc[1].clone(), x[1].clone()].concat()]
+                    });*/
             SeedMap { mapping: numbers }
         })
         .collect();
